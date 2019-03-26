16d662adaf3f33b4a4db6dcb97b1d5c7d4469b71
rafameureer <rafameureer@gmail.com>
2019-03-26 20:57:18 -0300

    Implementação do exercicio 3

--- ListTree/src/Main.java
+++ ListTree/src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Number02 programList = new Number02();
-		// Select number03 = new Select();
+		// Number02 programList = new Number02();
+		Select number03 = new Select();
 	}
 
 }
